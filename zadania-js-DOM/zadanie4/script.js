6fc4cee5f7870b51d529b7a025085e0cc411dfea
anstyrna <aniakatarzynas@gmail.com>
2021-05-17 00:10:35 +0200

    skończony warsztat

---
+++ zadania-js-DOM/zadanie4/script.js
@@ -0,0 +1,13 @@
+const submit = (event) => {
+    event.preventDefault();
+
+    let fName = document.querySelector('[name="fname"]');
+    let lName = document.querySelector('[name="lname"]');
+
+    console.log('Imię: ' + fName.value);
+    console.log('Nazwisko: ' + lName.value)
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submit);
